1d28817f9e2ad0895001c2334c82cdb043414f85
Akado2009 <prosvirov.k@gmail.com>
2018-09-11 10:38:23 -0700

    final fix

--- examples/mpi-mnist/cpu/mpi_mnist_cpu.py
+++ examples/mpi-mnist/cpu/mpi_mnist_cpu.py
@@ -115,7 +115,7 @@ def run(rank, size):
             optimizer.zero_grad()
             output = model(data)
             loss = F.nll_loss(output, target)
-            epoch_loss += loss.data[0]
+            epoch_loss += loss.item()
             loss.backward()
             average_gradients(model)
             optimizer.step()
