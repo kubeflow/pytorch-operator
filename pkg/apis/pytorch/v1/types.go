5a8b39ee82586024e464bd3d7a452613948b5b6a
Richard Liu <ricliu@google.com>
2019-05-24 15:07:14 -0700

    Polish documentation for PyTorch V1

--- pkg/apis/pytorch/v1/types.go
+++ pkg/apis/pytorch/v1/types.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Kubeflow Authors
+// Copyright 2019 The Kubeflow Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,49 +23,46 @@ import (
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +resource:path=pytorchjob
 
-// PyTorchJob represents the configuration of PyTorchJob
+// Represents a PyTorchJob resource.
 type PyTorchJob struct {
+	// Standard Kubernetes type metadata.
 	metav1.TypeMeta `json:",inline"`
 
-	// Standard object's metadata.
+	// Standard Kubernetes object's metadata.
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// Specification of the desired behavior of the PyTorchJob.
+	// Specification of the desired state of the PyTorchJob.
 	Spec PyTorchJobSpec `json:"spec,omitempty"`
 
 	// Most recently observed status of the PyTorchJob.
-	// This data may not be up to date.
-	// Populated by the system.
-	// Read-only.
+	// Read-only (modified by the system).
 	Status common.JobStatus `json:"status,omitempty"`
 }
 
 // PyTorchJobSpec is a desired state description of the PyTorchJob.
 type PyTorchJobSpec struct {
-	// Specifies the duration in seconds relative to the startTime that the job may be active
-	// before the system tries to terminate it; value must be positive integer.
-	// This method applies only to pods with restartPolicy == OnFailure or Always.
+	// Specifies the duration (in seconds) since startTime during which the job can remain active
+	// before it is terminated. Must be a positive integer.
+	// This setting applies only to pods where restartPolicy is OnFailure or Always.
 	// +optional
 	ActiveDeadlineSeconds *int64 `json:"activeDeadlineSeconds,omitempty"`
 
-	// Optional number of retries before marking this job failed.
+	// Number of retries before marking this job as failed.
 	// +optional
 	BackoffLimit *int32 `json:"backoffLimit,omitempty"`
 
-	// CleanPodPolicy defines the policy to kill pods after PyTorchJob is
-	// succeeded.
-	// Default to Running.
+	// Defines the policy for cleaning up pods after the PyTorchJob completes.
+	// Defaults to Running.
 	CleanPodPolicy *common.CleanPodPolicy `json:"cleanPodPolicy,omitempty"`
 
-	// TTLSecondsAfterFinished is the TTL to clean up pytorch-jobs (temporary
-	// before kubernetes adds the cleanup controller).
+	// Defines the TTL for cleaning up finished PyTorchJobs (temporary
+	// before Kubernetes adds the cleanup controller).
 	// It may take extra ReconcilePeriod seconds for the cleanup, since
 	// reconcile gets called periodically.
-	// Default to infinite.
+	// Defaults to infinite.
 	TTLSecondsAfterFinished *int32 `json:"ttlSecondsAfterFinished,omitempty"`
 
-	// PyTorchReplicaSpecs is map of PyTorchReplicaType and PyTorchReplicaSpec
-	// specifies the PyTorch replicas to run.
+	// A map of PyTorchReplicaType (type) to ReplicaSpec (value). Specifies the PyTorch cluster configuration.
 	// For example,
 	//   {
 	//     "Master": PyTorchReplicaSpec,
@@ -74,7 +71,7 @@ type PyTorchJobSpec struct {
 	PyTorchReplicaSpecs map[PyTorchReplicaType]*common.ReplicaSpec `json:"pytorchReplicaSpecs"`
 }
 
-// PyTorchReplicaType is the type for PyTorchReplica.
+// PyTorchReplicaType is the type for PyTorchReplica. Can be one of "Master" or "Worker".
 type PyTorchReplicaType common.ReplicaType
 
 const (
@@ -90,6 +87,7 @@ const (
 
 // PyTorchJobList is a list of PyTorchJobs.
 type PyTorchJobList struct {
+	// Standard type metadata.
 	metav1.TypeMeta `json:",inline"`
 
 	// Standard list metadata.
